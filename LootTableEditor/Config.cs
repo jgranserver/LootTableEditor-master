d05be1d5badd71dc5eb9bf063c073ef566c9b38c
C0sm0s-Chill <84866637+C0sm0s-Chill@users.noreply.github.com>
2021-05-26 19:10:53 +0200

    Add files via upload

---
+++ LootTableEditor/Config.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace LootTableEditor
+{
+	public class Config
+	{
+		public Dictionary<int, DropReplacement> LootReplacements;
+
+		public Config()
+		{
+			LootReplacements = new Dictionary<int, DropReplacement>();
+		}
+
+		public void WriteConfig(String file)
+		{
+			DropReplacement replacement = new DropReplacement();
+			replacement.drops = new Dictionary<State, List<Drop>>();
+			List<Drop> drops = new List<Drop>();
+			drops.Add(new Drop { chance = 1.0f, high_stack = 99, low_stack = 1, itemID = 73 });
+			replacement.drops.Add(State.Normal, drops);
+			LootReplacements.Add(3, replacement);
+
+			using (var tw = new StreamWriter(file))
+			{
+				tw.Write(JsonConvert.SerializeObject(LootReplacements, Formatting.Indented));
+			}
+		}
+
+		public void ReadFile(String file)
+		{
+			if (!File.Exists(file))
+				WriteConfig(file);
+
+			using (var tr = new StreamReader(file))
+			{
+				String raw = tr.ReadToEnd();
+				LootReplacements = JsonConvert.DeserializeObject<Dictionary<int, DropReplacement>>(raw);
+			}
+		}
+	}
+}
---
+++ LootTableEditor/DropReplacement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LootTableEditor
+{
+	public class DropReplacement
+	{
+		public Dictionary<State, List<Drop>> drops;
+		public bool tryEachItem = true;
+		public bool alsoDropDefaultLoot = false;
+
+		public DropReplacement()
+		{
+			drops = new Dictionary<State, List<Drop>>();
+		}
+	}
+
+	public enum State
+	{
+		Normal,
+		Bloodmoon,
+		Eclipse,
+		Night,
+		Day,
+		Fullmoon
+	}
+
+	public class Drop
+	{
+		public float chance = 1.0f;
+
+		public int low_stack = 0;
+		public int high_stack = 1;
+
+		public int itemID;
+
+		public int prefix;
+
+		public Drop()
+		{
+
+		}
+	}
+}
